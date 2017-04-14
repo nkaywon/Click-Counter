48e37e4449f0798ee5f879d721a40acc7836238e
nkaywon <hellonkaywon@gmail.com>
2017-04-15 02:25:07 +0300

    da
    da

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var path = require('path');
-var childProcess = require('child_process');
-var phantomjs = require('phantomjs');
-var binPath = phantomjs.path;
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var request = require('request');
 
-var childArgs = [
-  path.join(__dirname, 'phantom.js')
-];
-
-app.get('/', function (request, response) {
-  	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		response.sendFile(__dirname + '/screenshot.png');
+app.get('/', function (req, response) {
+  	request('https://2ip.ru', function (error, res, body) {
+  		console.log('vasa');
+	  response.send(body);
 	});
-})
+});
 
 app.listen(3000, function(){});
